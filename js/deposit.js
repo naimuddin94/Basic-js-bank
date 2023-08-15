89fc361364278d7458190d71f69059f81a261e25
naimuddin94 <developernaimuddin@gmail.com>
2023-08-16 00:20:41 +0600

    Basic validation facality add

--- js/deposit.js
+++ js/deposit.js
@@ -1,11 +1,14 @@
 document.getElementById("btn-deposit").addEventListener("click", function () {
   const depositAmountString = getInputValue("deposit-field");
   const depositAmount = parseFloat(depositAmountString);
+  if (isNaN(depositAmount)) {
+    return alert("Please enter valid deposit amount");
+  }
   const previewsDepositAmount = getElementValue("deposit-total");
   const depositTotalAmount = depositAmount + previewsDepositAmount;
-    setElementValue("deposit-total", depositTotalAmount);
-    
-    const totalBalance = getElementValue("balance-total");
-    const newTotalBalance = totalBalance + depositAmount;
-    setElementValue("balance-total", newTotalBalance);
+  setElementValue("deposit-total", depositTotalAmount);
+
+  const totalBalance = getElementValue("balance-total");
+  const newTotalBalance = totalBalance + depositAmount;
+  setElementValue("balance-total", newTotalBalance);
 });
--- js/withdraw.js
+++ js/withdraw.js
@@ -1,11 +1,16 @@
 document.getElementById("btn-withdraw").addEventListener("click", function () {
   const withdrawAmountString = getInputValue("withdraw-field");
   const withdrawAmount = parseFloat(withdrawAmountString);
+  const totalBalance = getElementValue("balance-total");
+  if (isNaN(withdrawAmount)) {
+    return alert("Please enter a valid amount");
+  } else if (withdrawAmount > totalBalance) {
+    return alert("You have not enough balance");
+  }
   const previewsWithdrawAmount = getElementValue("withdraw-total");
   const withdrawTotalAmount = withdrawAmount + previewsWithdrawAmount;
   setElementValue("withdraw-total", withdrawTotalAmount);
 
-  const totalBalance = getElementValue("balance-total");
   const newTotalBalance = totalBalance - withdrawAmount;
   setElementValue("balance-total", newTotalBalance);
 });
